b8f7af8d7ad63fc9b90cef1428acdd1f010cd25b
severac <tristanseverac@gmail.com>
2016-03-24 16:54:33 +0100

    commit goldenMaster.txt

--- C#/Trivia/Trivia/GameRunner.cs
+++ C#/Trivia/Trivia/GameRunner.cs
@@ -14,31 +14,35 @@ namespace Trivia
 
         public static void Main(String[] args)
         {
-            Game aGame = new Game();
+             for(int i=0; i<100;i++)
+             {
+                 Game aGame = new Game();
 
-            aGame.add("Chet");
-            aGame.add("Pat");
-            aGame.add("Sue");
+                aGame.add("Chet");
+                aGame.add("Pat");
+                aGame.add("Sue");
 
-            Random rand = new Random();
+                var rand = new Random(i);
 
-            do
-            {
-
-                aGame.roll(rand.Next(5) + 1);
-
-                if (rand.Next(9) == 7)
+                do
                 {
-                    notAWinner = aGame.wrongAnswer();
-                }
-                else
-                {
-                    notAWinner = aGame.wasCorrectlyAnswered();
-                }
 
+                    aGame.roll(rand.Next(5) + 1);
 
+                    if (rand.Next(9) == 7)
+                    {
+                        notAWinner = aGame.wrongAnswer();
+                    }
+                    else
+                    {
+                        notAWinner = aGame.wasCorrectlyAnswered();
+                    }
 
-            } while (notAWinner);
+                } 
+                while (notAWinner);
+
+             }
+            
 
         }
 
