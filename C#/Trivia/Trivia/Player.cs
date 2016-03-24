f074e7cc2df80a3957c805b78271787141ee7cd7
severac <tristanseverac@gmail.com>
2016-03-24 17:53:16 +0100

    commit classe Player

---
+++ C#/Trivia/Trivia/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Trivia
+{
+    class Player
+    {
+        private String playerName;
+
+        public Player(string playerName)
+        {
+            this.playerName = playerName;
+        }
+    }
+}
